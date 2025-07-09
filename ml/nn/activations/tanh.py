0a162d669be34b792e77a9f1729faeca7fc7157b
jonatanprepuk <jonatanprepuk@gmail.com>
2025-07-09 19:54:13 +0200

    small fixes in tanh

--- ml/nn/activations/tanh.py
+++ ml/nn/activations/tanh.py
@@ -4,11 +4,11 @@ from .base import ActivationLayer
 class Tanh(ActivationLayer):
     def forward(self, inputs: np.ndarray, training: bool) -> None:
         self.inputs = inputs
-        self.output = np. tanh(inputs) # (np.exp(inputs) -np.exp(-inputs)) / (np.exp(inputs) + np.exp(-inputs))
+        self.output = np.tanh(inputs) # (np.exp(inputs) -np.exp(-inputs)) / (np.exp(inputs) + np.exp(-inputs))
     
-    def backward(self, dvalues):
+    def backward(self, dvalues: np.ndarray) -> None:
         self.dinputs = dvalues * (1 - self.output ** 2)
     
-    def predictions(self, outputs):
+    def predictions(self, outputs: np.ndarray) -> np.ndarray:
         return (outputs > 0) * 1
     
